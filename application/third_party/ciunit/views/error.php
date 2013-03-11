7b5633fd342f3717c765da3826485bbb2d92da7f
Agop Seropyan <agopseropyan@gmail.com>
2013-03-11 18:49:46 +0000

    Removing boostrap-responsive.css from views

--- application/third_party/ciunit/views/error.php
+++ application/third_party/ciunit/views/error.php
@@ -10,10 +10,7 @@
 <!-- Le styles -->
 <link
 	href="<?php print base_url() . $resources_path . "css/bootstrap.css" ; ?>"
-	rel="stylesheet">
-<link
-	href="<?php print base_url() . $resources_path . "css/bootstrap-responsive.css" ; ?>"
-	rel="stylesheet">
+	rel="stylesheet">  
 <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
 <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
