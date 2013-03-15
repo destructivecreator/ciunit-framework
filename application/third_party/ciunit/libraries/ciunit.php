de9f1c3bb57b9e735ca902b23d9fc3fb4e13b00f
Agop Seropyan <agopseropyan@gmail.com>
2013-03-15 22:12:39 +0000

    Updating version in views

--- application/third_party/ciunit/libraries/ciunit.php
+++ application/third_party/ciunit/libraries/ciunit.php
@@ -3,7 +3,7 @@
 // Load the autoload file for t
 require_once APPPATH . "third_party/ciunit/framework/autoload.php";
 
-define('CIUNIT_VERSION', '1.1 Beta');
+define('CIUNIT_VERSION', '1.2 Beta');
 
 /**
  * CIUnit library class to use with CodeIgniter
